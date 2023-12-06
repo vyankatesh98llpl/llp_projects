53b8a0c183d00fe58f2a73715083c70f34cb9df8
vyankatesh98llpl <vyankatesh98llp@gmail.com>
2023-12-06 17:17:46 +0530

    javascript add , sub,mul,div opertion is done and current update next code logics

--- calci.js
+++ calci.js
@@ -1,11 +1,22 @@
 // console.log("Hello, world!");
 const prompt=require("prompt-sync")({sigint:true}); 
-var user=parseInt(prompt("enter the first Number? "));
-var opertion=parseInt(prompt("enter the option 1.Addition:-+ \n 2.substration:-- \n3.multiplication:-* \n4.Divion:-"));
-var user1=prompt("enter the second Number?");
-console.log(user);
-console.log(opertion);
-console.log(user1);
-
-
-// println(user, opertion,user1)
+var user=parseInt(prompt("enter the first Number:- "));
+var opertion=prompt("enter the option 1.Addition:-+ \n 2.substration:-- \n3.multiplication:-* \n4.Divion \n:-");
+var user1=parseInt(prompt("enter the second Number:- "));
+// console.log(user);
+// console.log(opertion);
+// console.log(user1);
+let ans
+if(opertion == "+"){
+    ans = user + user1;
+}
+else if(opertion == "-"){
+    ans = user - user1
+}
+else if(opertion == "*"){
+    ans = user * user1
+}
+else if(opertion == "/"){
+    ans = user / user1
+}
+console.log(user, opertion , user1 ,'=',ans);
