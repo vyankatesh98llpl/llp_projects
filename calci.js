02a9a496916f5b4c1738f8cbbd677849ed560d52
vyankatesh98llpl <vyankatesh98llp@gmail.com>
2023-12-06 18:28:18 +0530

    in js file update the code use function and run the code

--- calci.js
+++ calci.js
@@ -1,22 +1,46 @@
 // console.log("Hello, world!");
 const prompt=require("prompt-sync")({sigint:true}); 
+
+function Addition(a,b){
+    result = a + b;
+    return result
+}
+function substration(a,b){
+    result = a - b;
+    return result
+}
+function multiplication(a,b){
+    result = a * b;
+    return result
+}
+function Divion(a,b){
+    result = a / b;
+    return result
+}
+
+let i=1
+while (i>0){
 var user=parseInt(prompt("enter the first Number:- "));
 var opertion=prompt("enter the option 1.Addition:-+ \n 2.substration:-- \n3.multiplication:-* \n4.Divion \n:-");
 var user1=parseInt(prompt("enter the second Number:- "));
-// console.log(user);
-// console.log(opertion);
-// console.log(user1);
-let ans
 if(opertion == "+"){
-    ans = user + user1;
-}
+        ans = Addition(user,user1);
+    }
 else if(opertion == "-"){
-    ans = user - user1
-}
+    ans =substration(user,user1);
+    }
 else if(opertion == "*"){
-    ans = user * user1
-}
+    ans =multiplication(user,user1);
+    }
 else if(opertion == "/"){
-    ans = user / user1
-}
+    ans =Divion(user,user1);
+    }
 console.log(user, opertion , user1 ,'=',ans);
+}
+
+
+
+
+
+
+
